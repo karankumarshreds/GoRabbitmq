f244b05031f970a05e9f29ed0a568392868f5458
karankumarshreds <naileditnerd@gmail.com>
2021-09-19 21:59:18 +0530

    add routing practice code

---
+++ practice/routing/pub/pub.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+	"github.com/streadway/amqp"
+)
+
+const connString = "amqp://guest:guest@localhost:5672/"
+
+func main() {
+
+	// dial a connection
+	conn, err := amqp.Dial(connString)
+	failOnError(err, "Unable to establish a connection")
+
+	// open a channel 
+	ch, err := conn.Channel()
+	failOnError(err, "Unable to open a channel")
+
+	// declare an exchange with its type 
+	err  = ch.ExchangeDeclare(
+		"direct_logs",    // name of the exchange 
+		"direct",         // type of exchange 
+		true,             // durable 
+		false,            // auto-delete 
+		false,            // internal
+		false,            // no-wait
+		nil,
+	)
+	failOnError(err, "Unable to declare an exchange")
+
+	log.Printf("Publishing the message to routing key === %v\n", routingKeyFromArgs(os.Args))
+	// publish the message on the exchange 
+	for i := 0; i < 200; i ++ {
+		ch.Publish(
+			"direct_logs",                        // name of exchange 
+			routingKeyFromArgs(os.Args),          // routing key (info|warning|error)
+			false,                                // mandatory
+			false,                                // immediate 
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body: []byte(fmt.Sprintf("👉 %v", i)),
+			},
+		)
+	}
+
+}
+
+// helper functions 
+
+func routingKeyFromArgs(args []string) string {
+	var s string
+		if (len(args) < 2) || os.Args[1] == "" {
+			s = "info"
+		} else {
+			s = os.Args[1]
+		}
+	return s
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+    log.Fatalf("%s: %s", msg, err)
+  }
+}
---
+++ practice/routing/sub-3/sub.go
@@ -0,0 +1,90 @@
+/*
+
+		LISTENS TO ROUTING KEY "error" PASSED IN ARGUMENTS BY PUBLISHER
+
+*/
+
+package main
+
+import (
+	"log"
+	"github.com/streadway/amqp"
+)
+
+const connString = "amqp://guest:guest@localhost:5672/"
+
+func main() {
+	
+	// dial a connection
+	conn, err := amqp.Dial(connString)
+	failOnError(err, "Unable to dial a connection")
+
+	// open a channel
+	ch, err := conn.Channel()
+	failOnError(err, "Unable to open a channel")
+
+	// declare a queue 
+	q, err := ch.QueueDeclare(
+		"error_listening_queue",          // name of the queue 
+		false,       // durable 
+		false,       // autoDelete 
+		false,        // exclusive 
+		false,       // noWait
+		nil,
+	)
+	failOnError(err, "Unable to declare a queue")
+	
+	// declare an exchange 
+	err = ch.ExchangeDeclare(
+		"direct_logs",
+		"direct",
+		true,  // durable if the server restarts 
+		false, // auto-deleted if the server restarts 
+		false, // internal exchanges do not accept the external publishings 
+		false, // no-wait (no-wait declare without confirmation from the server)
+		nil,
+	)
+	failOnError(err, "Unable to declare exchange")
+
+	// bind the exchange 
+	err = ch.QueueBind(
+		q.Name,           // name of the queue 
+		"error",          // name of the key
+		"direct_logs",    // name of the exchange 
+		false,
+		nil,
+	)
+	failOnError(err, "Unable to bind the exchange")
+
+	// listen for the messages 
+	msgs, err := ch.Consume(
+		q.Name,
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Unable to consume messages")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf(" [x] %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for logs. To exit press CTRL+C")
+	<-forever
+
+}
+
+// helper functions 
+
+func failOnError(err error, msg string) {
+	if err != nil {
+    log.Fatalf("%s: %s", msg, err)
+  }
+}
