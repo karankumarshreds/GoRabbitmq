628e3173e349b81c5aa98dd3ae2cb73f464c69ad
karankumarshreds <naileditnerd@gmail.com>
2021-09-20 23:59:43 +0530

    add design pattern

---
+++ pattern/listen/listen.go
@@ -0,0 +1,71 @@
+package listen 
+
+import (
+	"github.com/streadway/amqp"
+)
+
+type listener struct {
+	Channel *amqp.Channel
+	consumer string
+}
+
+// New will create a new instance of listener 
+func New(ch *amqp.Channel, consumer string) listener{
+	return listener{
+		Channel: ch,
+		consumer: consumer,
+	}
+}
+
+func (l listener) Listen(queue string, exchange string, topic string) (msgs <-chan amqp.Delivery, err error) {
+	q, err := l.Channel.QueueDeclare(
+		queue,   // queue name 
+		true,    // durable 
+		false,   // autoDelete
+		false,   // exclusive
+		false,   // noWait 
+		nil,
+	)
+	returnIfError(err)
+
+	err = l.Channel.ExchangeDeclare(
+		exchange,  // exchange name 
+		"direct",  // type
+		true,      // durable 
+		false,     // autoDelete
+		false,     // internal 
+		false,     // noWait 
+		nil,
+	)
+	returnIfError(err)
+
+	err = l.Channel.QueueBind(
+		q.Name,
+		topic,
+		exchange,
+		false,
+		nil,
+	)
+	
+
+	msgs, err = l.Channel.Consume(
+		q.Name,
+		l.consumer,
+		true,  // auto-ack
+		false, // exclusive
+		false, // no-local
+		false, // no-wait 
+		nil,   // args 
+	)
+
+	returnIfError(err)
+	
+}
+
+func returnIfError(err error) (interface{}, error) {
+	if err != nil {
+		return nil, err
+	} 
+}
+
+
